94e936e449e75158d4bd0065efbbc7d15672ef8d
zohaib <zohaibmuhammad025@gmail.com>
2022-02-23 07:00:09 +0500

    updated

--- csvhandler/settings.py
+++ csvhandler/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'bgv1cu*1pttcqfe=cxe4fpecy)1b3=qo%(!zj_-ex%7ei=9a3w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
